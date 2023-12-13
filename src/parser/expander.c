df5a7050740fe3370fc0ab95ab281256b6c877ef
Kirill Kopnev <kkopnev@f1r6s15.codam.nl>
2023-12-13 17:53:00 +0100

    added jiajia files

--- src/parser/expander.c
+++ src/parser/expander.c
@@ -6,9 +6,133 @@
 /*   By: jiajchen <jiajchen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/08 16:39:12 by jiajchen      #+#    #+#                 */
-/*   Updated: 2023/12/11 16:48:13 by jiajchen      ########   odam.nl         */
+/*   Updated: 2023/12/13 17:35:45 by kkopnev       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+
+/* find the variable in the env. Never return NULL, only can be empty string!! */
+char	*find_variable(char *var, char **env)
+{
+	int		i;
+	char	*content;
+
+	i = 0;
+	var = ft_strjoin(var, "=");
+	while (env[i] && ft_strncmp(env[i], var, ft_strlen(var)) != 0)
+		i++;
+	if (env[i])
+		content = ft_strdup(env[i] + ft_strlen(var));
+	else
+		content = ft_calloc(sizeof(char), 1);
+	if (!content)
+		perror("malloc"); // todo: write another error() to exit(0)
+	if (!env[i])
+		content[0] = '\0';
+	return (content);
+}
+
+t_lexer	*ft_lexsplit(t_lexer **lst, t_lexer *lexer, char *str)
+{
+	char	**tabs;
+	t_lexer	*node;
+	int		i;
+
+	tabs = ft_split(str, ' ');
+	i = -1;
+	while (tabs[++i])
+	{
+		node = ft_lexnew(tabs[i], WORD);
+		ft_lexinsert(lst, lexer->prev, lexer, node);
+	}
+	ft_lexdel(ft_lexretract(lst, lexer));
+	free(tabs); //only free the crust? not strings
+	free(str);
+	return (node);
+}
+
+/* return 1 if prev lexer is '<<' and not in the dquotes*/
+int	is_dredir(t_lexer *lexer)
+{
+	t_lexer	*tmp;
+	
+	tmp = lexer->prev;
+	if (tmp->state != GENERAL)
+		return (0);
+	if (tmp)
+	{
+		if (tmp->token == DREDIR_OUT)
+			return (1);
+		if (tmp->token != WHITE_SPACE)
+			return (0);
+		tmp = tmp->prev;
+		if (tmp && tmp->token == DREDIR_OUT)
+			return (1);
+	}
+	return (0);
+}
+
+char	*ft_expand_env(char *vars, char **env, int exit_c)
+{
+	char	**tabs;
+	char	*contents;
+	char	*tmp;
+	int		i;
+
+	tabs = ft_split(vars, '$');
+	i = -1;
+	contents = NULL;
+	while (tabs[++i])
+	{
+		if (tabs[i][0] == '?')
+		{
+			tmp = ft_itoa(exit_c);
+			contents = ft_strjoin_free(contents, tmp);
+			contents = ft_strjoin_free(contents, tabs[i] + 1);
+		}
+		else
+		{
+			tmp = find_variable(tabs[i], env);
+			contents = ft_strjoin_free(contents, tmp);
+		}
+		free(tmp);
+	}
+	free_arr(tabs);
+	return (contents);
+}
+
+/* echo $?123$USER -- 0123jiajchen*/
+void	expand_env(t_lexer **lst, char **env, int exit_c)
+{
+	char	*tmp;
+	t_lexer	*lexer;
+	
+	lexer = *lst;
+	while (lexer)
+	{
+		if (lexer->token == ENV && lexer->state != IN_QUOTE && !is_dredir(lexer))
+		{
+			tmp = ft_expand_env(lexer->content, env, exit_c);
+			if (ft_strchr(tmp, ' ') && lexer->state == GENERAL)
+				lexer = ft_lexsplit(lst, lexer, tmp);
+			else
+			{
+				free(lexer->content);
+				lexer->content = tmp;
+				lexer->len = ft_strlen(tmp);
+			}
+		}
+		// if (lexer->token == ENV)
+		// 	lexer->token = WORD;
+		lexer = lexer->next;
+	}
+}
+
+
+
+
 
 /**
  * After doing Lexer we have a linked list as an output.
---
+++ src/parser/parcer_utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parcer_utils.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jiajchen <jiajchen@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/12/12 10:28:30 by jiajchen      #+#    #+#                 */
+/*   Updated: 2023/12/13 17:36:21 by kkopnev       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+t_cmd*	ft_cmdnew(void)
+{
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (cmd == NULL)
+		return (NULL);
+	cmd->args = NULL;
+    // int				(*builtin)(t_tools *, struct s_cmd *);
+    cmd->num_redir = 0;
+	cmd->redir= NULL;
+    cmd->prev = NULL;
+    cmd->next = NULL;
+	return (cmd);
+}
+
+void	ft_cmdaddfront(t_cmd** cmd, t_cmd* new)
+{
+    if (new == NULL)
+    {
+        return ;
+    }
+	new->next = *cmd;
+    if (*cmd != NULL)
+    {
+        (*cmd)->prev = new;
+    }
+	*cmd = new;
+}
+
+void	ft_cmdaddback(t_cmd **cmd, t_cmd *new)
+{
+    t_cmd* last;
+
+    last = NULL;
+	if ((*cmd) == NULL)
+		ft_cmdaddfront(cmd, new);
+	else
+    {
+        last = ft_cmdlast(*cmd);
+		last->next = new;
+        new->prev = last; 
+        new->next = NULL;
+    }
+}
+
+t_cmd*	ft_cmdlast(t_cmd* cmd)
+{
+	if (cmd == NULL)
+		return (NULL);
+	while (cmd->next != NULL)
+		cmd = cmd->next;
+	return (cmd);
+}
+
+void	ft_cmddel(t_cmd* node)
+{
+    int i;
+
+    i = -1;
+    if (node->args)
+    {
+        while((node->args)[++i])
+            free((node->args)[i]);
+    }
+    free(node->args);   
+	free(node);
+}
+
+void    ft_cmdclean(t_cmd** cmd)
+{
+    t_cmd*    temp;
+    
+    temp = NULL;
+    while(*cmd)
+    {   
+        temp = *cmd;
+        *cmd = (*cmd)->next;
+        ft_cmddel(temp);
+    }
+}
