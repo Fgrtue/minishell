866c584319e2b98ae8e5ae81e8f17e6a99d9f2aa
Kirill Kopnev <kkopnev@f1r4s5.codam.nl>
2024-01-22 19:23:15 +0100

    fixed echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: kkopnev <kkopnev@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/22 10:36:14 by kkopnev       #+#    #+#                 */
-/*   Updated: 2024/01/17 20:38:15 by kkopnev       ########   odam.nl         */
+/*   Updated: 2024/01/22 19:22:53 by kkopnev       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,38 @@
 
 */
 
-int ft_echo(t_cmd* cmd, char ***env, t_global* global)
+static int check_option(char*	arg)
 {
-	int     mode;
-	int     i;
-	char*   line;
+	int	i;
 
-	(void) env;
-	global = NULL;
+	i = 0;
+	if (arg[i++] != '-')
+		return (1);
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return(1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_echo(t_cmd *cmd, t_global *global)
+{
+	int		mode;
+	int		i;
+	char	*line;
+
 	i = 1;
 	mode = 0;
 	line = NULL;
-	if ((cmd->args)[1]) 
+	if (global && (cmd->args)[1])
 	{
-		mode = ft_strncmp((cmd->args)[1], "-n", ft_strlen((cmd->args)[1]));
-		if (mode == 0)
+		while((cmd->args)[i] && check_option((cmd->args)[i]) != 1)
+		{
 			i++;
+			mode = 1;
+		}
 		while ((cmd->args)[i])
 		{
 			line = ft_strjoin_free(line, (cmd->args)[i++]);
@@ -51,8 +67,7 @@ int ft_echo(t_cmd* cmd, char ***env, t_global* global)
 		}
 		write((cmd->fd_io)[1], line, ft_strlen(line));
 	}
-	if (mode != 0)
-		write((cmd->fd_io)[1], "\n", 1);
+	write((cmd->fd_io)[1], "\n", !mode);
 	free(line);
 	return (0);
 }
